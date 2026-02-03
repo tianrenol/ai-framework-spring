4ce60b50a40c8ca1906b11dafb3caa27a43e1ea5
tianrenol <92942741+tianrenol@users.noreply.github.com>
2026-02-03 19:33:59 +0800

    init

---
+++ src/test/java/com/tianrenservice/ai_framework_spring/AiFrameworkSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tianrenservice.ai_framework_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiFrameworkSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
